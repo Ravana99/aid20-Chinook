e14e43c7e212feaad10bbef924a4332cb0ac6bae
aid <tomas.lopes@tecnico.ulisboa.pt>
2020-11-21 15:36:43 +0000

    minor tweaks to challenge 2, 3 and 6

--- 06/queries.sql
+++ 06/queries.sql
@@ -1,9 +1,9 @@
 use Chinook_dw;
 
-select Country, City, YearId, sum(LineTotal)
+select Country, City, YearId, sum(LineTotal) as sum_sales
 from fact_sales natural join dim_customer natural join dim_time
 group by Country, City, YearId;
 
-select ArtistName, Country, YearId, sum(quantity)
+select ArtistName, Country, YearId, sum(quantity) as sum_quantity
 from fact_sales natural join dim_track natural join dim_customer natural join dim_time
 group by ArtistName, Country, YearId;
